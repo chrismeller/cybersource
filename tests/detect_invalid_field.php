b54e5a8b4b68c49fc62c5197b3f4e9756470f9a6
Chris Meller <chris@doesnthaveone.com>
2012-02-09 16:27:13 -0500

    Add two new tests for missing and invalid fields, which we're now checking for.

---
+++ tests/detect_invalid_field.php
@@ -0,0 +1,29 @@
+<?php
+
+	require('main.php');
+	
+	$c->card( '4111111111111111', '12', '2013', '123' )
+		->bill_to( array(
+			'firstName' => 'John',
+			'lastName' => 'Tester',
+			'street1' => '123 Main Street',
+			'city' => 'Columbia',
+			'state' => 'SC',
+			'postalCode' => '29201654654654',
+			'country' => 'US',
+			'email' => 'john.tester@example.com',
+		) );
+	
+	try {
+		$c->authorize( 1 );
+	}
+	catch ( CyberSource_Invalid_Field_Exception $e ) {
+		echo $e->getMessage();
+	}
+	
+	echo '<pre>';
+	print_r( $c->request );
+	print_r( $c->response );
+	echo '</pre>';
+
+?>
