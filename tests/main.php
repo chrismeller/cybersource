db84adcc8d88db191b36a7e54561288e6f05bb04
Chris Meller <cmeller@capsoftinc.com>
2012-06-06 16:23:22 -0400

    Include the CyberSource_Reporting class as well.

--- tests/main.php
+++ tests/main.php
@@ -1,9 +1,10 @@
 <?php
 
 	require('../cybersource.php');
-	
+	require('../cybersource_reporting.php');
+
 	require('config.php');
-	
+
 	$c = CyberSource::factory( $merchant_id, $transaction_id, CyberSource::ENV_TEST );
 
 ?>
