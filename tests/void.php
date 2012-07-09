51b50d4cbe24c802d129ac1658079ce0b9289720
Chris Meller <cmeller@capsoftinc.com>
2012-07-09 11:18:50 -0400

    Add a quick test that uses the charge test to perform a charge, then immediately voids it.

---
+++ tests/void.php
@@ -0,0 +1,17 @@
+<?php
+
+	// require the charge test, which charges a number of items that we'll then void
+	require('charge.php');
+
+	// get the request id
+	$request_id = $c->response->requestID;
+
+	// perform the void
+	$c->void( $request_id );
+
+	echo '<pre>';
+	print_r( $c->request );
+	print_r( $c->response );
+	echo '</pre>';
+
+?>
