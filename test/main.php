b315ddd407ace09f29b3f2f230cd475368751d69
Junlapong L <junlapong@gmail.com>
2017-11-14 15:58:01 +0700

    change EOL to EOF

--- test/main.php
+++ test/main.php
@@ -7,6 +7,6 @@ require realpath(dirname( __FILE__ ) . '/../classes/CyberSource/CyberSource.php'
 require realpath(dirname( __FILE__ ) . '/config.php');
 
 $c = CyberSource\CyberSource::factory($merchant_id, $transaction_key, CyberSource\CyberSource::ENV_TEST);
-$c->set_proxy($proxy);
+// $c->set_proxy($proxy);
 
 // EOF
